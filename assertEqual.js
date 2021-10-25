351e59734f151fd8e5257f4845df5616e1b27e06
Joseph Micla <josephmicla@gmail.com>
2021-10-25 22:21:10 +0000

    implemented assertEqual with colourful emojis

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✨Assertion Passed✨: " + actual + " === " + expected);
+  } else {
+    console.log("🛑Assertion Failed🛑: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("match", "match");
+assertEqual(1, 1);
+assertEqual(1, 2);

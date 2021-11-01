2b7584137380888144b504ebc7ff35583fe2678c
Joseph Micla <josephmicla@gmail.com>
2021-11-01 23:35:42 +0000

    installed mocha and chai and used them for testing in headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,27 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["one"]), "one");
-assertEqual(head([]), undefined);
+
+// Old Test Cases
+// assertEqual(head([5,6,7]), 5);
+// 
+// assertEqual(head(["one"]), "one");
+// 
+
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  })
+
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it('returns the middle element for an array of strings', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it('returns undefind when given an empty arr', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});

baf711841a58c3e1d84a996a2ee81d07a374c586
Joseph Micla <josephmicla@gmail.com>
2021-11-02 00:05:22 +0000

    refactored middleTest.js to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,36 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); //  odd numbered length
-assertArraysEqual(middle(["1", "2", "3", "4", "5", "6", "7"]), ["4"]); //  odd numbered length string
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); //  even numbered length
-assertArraysEqual(middle(["1", "2", "3", "4", "5", "6"]), ["3", "4"]); //  even numbered length string
-assertArraysEqual(middle([1, 2]), []); // two element case
-assertArraysEqual(middle([1]), []); // single element case
-assertArraysEqual(middle([]), []); // empty arr case
+describe('#middle', () => {
+  it('should return 1 element for an odd numbered array of strings', () => {
+    assert.deepEqual(middle(["1", "2", "3", "4", "5", "6", "7"]), ["4"]); //  odd numbered length string
+  });
+
+  it('should return 1 element for an odd numbered array of nums', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); //  odd numbered length
+  }); 
+
+  it('should return 2 elements for an even numbered array of nums', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); //  even numbered length
+  });
+
+  it('should return 2 elements for an even numbered array of strings', () => {
+    assert.deepEqual(middle(["1", "2", "3", "4", "5", "6"]), ["3", "4"]); //  even numbered length string
+  });
+
+  it('should return [] when there are two elements', () => {
+    assert.deepEqual(middle([1, 2]), []); // two element case
+  });
+
+  it('should return [] when there is a single element', () => {
+    assert.deepEqual(middle([1]), []); // single element case\
+  });
+
+  it('should return [] when passed []', () => {
+    assert.deepEqual(middle([]), []); // empty arr case
+  });
+})
+
+
+
+

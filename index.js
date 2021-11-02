3419beac6c16a49e3f7d44c0618af48f7c1b8d29
Joseph Micla <josephmicla@gmail.com>
2021-11-02 00:36:28 +0000

    required all utility functions into index.js and exported all as methods

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 
 module.exports = {
@@ -18,4 +26,12 @@ module.exports = {
   countLetters,
   countOnly,
   eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
 };
